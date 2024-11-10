00af3104b79462c1e271dc66c9c7bf6759e6e98a
magali <z1915336@students.niu.edu>
2024-11-09 21:27:54 -0600

    Initial Commit

---
+++ Assignment4/Assignment4App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment4App.swift
+//  Assignment4
+//
+//  Created by Magali Gutierrez on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
